c0ed4139d9f0bb72002c9678afb4325d4e279dc7
Charles A Edwards <charles.a.edwards@btinternet.com>
2018-06-21 18:36:08 +0100

    Test for successful tank possession implemented (127)
    Added a test in C++ to make sure that player takes control of a tank as
    soon as they enter the game.

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,6 +3,23 @@
 #include "TankPlayerController.h"
 #include "BattleTank.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController BeginPlay"));
+
+	ATank* PossessedTank = GetControlledTank();
+	if (GetControlledTank() == nullptr)
+	{
+		PossessedTank->GetName();
+		UE_LOG(LogTemp, Warning, TEXT("Tank not possessed"));
+	}
+	else
+	{
+
+		UE_LOG(LogTemp, Warning, TEXT("Tank possessed: %s"), *PossessedTank->GetName());
+	}
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
