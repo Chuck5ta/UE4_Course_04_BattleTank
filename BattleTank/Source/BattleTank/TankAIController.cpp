3532df84a59475aff201ccd15a91a06803e64554
Charles A Edwards <charles.a.edwards@btinternet.com>
2018-06-22 11:06:01 +0100

    The AI can now find the player
    AI detection of the player has now been implemented in C++

--- BattleTank/Source/BattleTank/TankAIController.cpp
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -2,28 +2,38 @@
 
 #include "TankAIController.h"
 #include "BattleTank.h"
+#include "Engine/World.h"
 
 void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Warning, TEXT("AI BeginPlay"));
+	UE_LOG(LogTemp, Warning, TEXT("AIController BeginPlay"));
 
-	ATank* PossessedTank = GetControlledTank();
-	if (GetControlledTank() == nullptr)
+	ATank* PlayerTank = GetPlayerTank();
+	if (PlayerTank == nullptr)
 	{
-		PossessedTank->GetName();
-		UE_LOG(LogTemp, Warning, TEXT("AI Tank not possessed"));
+		PlayerTank->GetName();
+		UE_LOG(LogTemp, Warning, TEXT("AIController cannot find player tank"));
 	}
 	else
 	{
-
-		UE_LOG(LogTemp, Warning, TEXT("AI Tank possessed: %s"), *PossessedTank->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("AIController found player: %s"), *PlayerTank->GetName());
 	}
 }
 
 ATank* ATankAIController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	APawn* PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn)
+	{
+		return nullptr;
+	}
+	return Cast<ATank>(PlayerPawn);
 }
 
 
