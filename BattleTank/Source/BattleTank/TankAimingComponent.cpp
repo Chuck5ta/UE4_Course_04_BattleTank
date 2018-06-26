60aa6281f93994f72a5431d870786b4a66978d14
Charles A Edwards <charles.a.edwards@btinternet.com>
2018-06-26 08:53:28 +0100

    Predict Projectile Landing Point completed (141)
    Completed lecture 141, "Predict Projectile Landing Point"

--- BattleTank/Source/BattleTank/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/TankAimingComponent.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+#include "Kismet/GameplayStatics.h"
 #include "TankAimingComponent.h"
 #include "Engine/World.h"
 
@@ -40,6 +41,27 @@ void UTankAimingComponent::TickComponent(float DeltaTime, ELevelTick TickType, F
 }
 
 void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
-{	
-	UE_LOG(LogTemp, Warning, TEXT("Firing at %f"), LaunchSpeed);
+{
+	if (!Barrel) { return; }
+
+	FVector OutLaunchVelocity;
+	FVector StartLocation = Barrel->GetSocketLocation(FName("Projectile"));
+	if (UGameplayStatics::SuggestProjectileVelocity(
+			this,
+			OutLaunchVelocity,
+			StartLocation,
+			HitLocation,
+			LaunchSpeed,
+			false,
+			0,
+			0,
+			ESuggestProjVelocityTraceOption::DoNotTrace
+			)
+		)
+	{
+		FVector AimDirection = OutLaunchVelocity.GetSafeNormal();
+		FString TankName = GetOwner()->GetName();
+		UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s"), *TankName, *AimDirection.ToString());
+	}
+	// If no solution found do nothing
 }
