5db2fa0283f07d80ade8b55acd304a9dbc2c0f42
Charles A Edwards <charles.a.edwards@btinternet.com>
2018-07-04 10:36:46 +0100

    Tank AI now fires projectiles (157)
    AI controlled tanks now fire projectiles.
    Projectile firing has been limited by the use of a timer.

--- BattleTank/Source/BattleTank/TankAIController.cpp
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -8,46 +8,24 @@
 void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Warning, TEXT("AIController BeginPlay"));
-
-	ATank* PlayerTank = GetPlayerTank();
-	if (PlayerTank == nullptr)
-	{
-		PlayerTank->GetName();
-		UE_LOG(LogTemp, Warning, TEXT("AIController cannot find player tank"));
-	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("AIController found player: %s"), *PlayerTank->GetName());
-	}
+	// UE_LOG(LogTemp, Warning, TEXT("AIController BeginPlay"));
 }
 
 void ATankAIController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	// Aim at player tank if it exists
-	if (GetPlayerTank())
-	{
-		GetControlledTank()->AimAt(GetPlayerTank()->GetActorLocation());
-	}
+	ATank* PlayerTank = Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
 
+	ATank* ControlledTank = Cast<ATank>(GetPawn());
+
+	if (PlayerTank)
+	{
+		ControlledTank->AimAt(Cast<ATank>(PlayerTank)->GetActorLocation());
+
+		ControlledTank->Fire(); // TODO limit firing rate
+	}
 	//UE_LOG(LogTemp, Warning, TEXT("TICK TOCK"));
-}
-
-ATank* ATankAIController::GetControlledTank() const
-{
-	return Cast<ATank>(GetPawn());
-}
-
-ATank* ATankAIController::GetPlayerTank() const
-{
-	APawn* PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
-	if (!PlayerPawn)
-	{
-		return nullptr;
-	}
-	return Cast<ATank>(PlayerPawn);
 }
 
 
--- BattleTank/Source/BattleTank/TankAIController.h
+++ BattleTank/Source/BattleTank/TankAIController.h
@@ -20,12 +20,6 @@ class BATTLETANK_API ATankAIController : public AAIController
 private:
 	virtual void BeginPlay() override;
 
-	virtual void Tick(float DeltaTime) override;
-	
-	ATank* GetControlledTank() const;
-
-	ATank* GetPlayerTank() const;
-
-	
+	virtual void Tick(float DeltaTime) override;	
 	
 };
